9fcb991a41725fb2d0b3cd996ccabcfc993dd4ec
bill <bill.arnold@student.moringaschool.com>
2023-09-29 23:56:49 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  // matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
